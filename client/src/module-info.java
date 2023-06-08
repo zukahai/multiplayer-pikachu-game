e08fe84c76ff57fde3ed87cb929733cfd8d8097b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-06-08 21:52:40 +0700

    init project

---
+++ client/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author HAIZUKA
+ *
+ */
+module client {
+}
---
+++ server/src/model/test.java
@@ -0,0 +1,12 @@
+package model;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		while (true) {
+			System.out.println("Hello 2");
+		}
+	}
+
+}
