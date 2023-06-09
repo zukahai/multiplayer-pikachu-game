d40d9f8073f91230e0028f3fccfc73aa3677e09b
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-06-09 18:52:07 +0700

    delete file trash

--- server/src/service/Util.java
+++ server/src/service/Util.java
@@ -2,6 +2,8 @@ package service;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Util {
 	public static String getIPv4() {
@@ -14,7 +16,27 @@ public class Util {
 		}
 		return IP.getHostAddress();
 	}
+	
+	public static Connection getConnection() {
+		Connection conn = null;
+		try{ 
+			   String userName = "root";
+			   String password = "";
+			   String databaseName = "pikachu";
+			   String url = "jdbc:mysql://localhost/" + databaseName;
+			   Class.forName ("com.mysql.cj.jdbc.Driver");
+			   conn = DriverManager.getConnection(url, userName, password);
+			   System.out.println("Connected datatabase " + databaseName);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		return conn;
+	}
+	
+
 	public static void main(String[] args) {
 		System.out.println(Util.getIPv4());
+		Util.getConnection();
 	}
 }
