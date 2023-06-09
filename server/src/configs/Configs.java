1e0fa537a156b8ac79e942b78f5f2d11f54bee57
Nam077 <nam077.me@gmail.com>
2023-06-09 20:55:11 +0700

    update config

---
+++ server/src/configs/Configs.java
@@ -0,0 +1,13 @@
+package configs;
+
+public class Configs {
+    // Config Mysql
+    public static final String MSQL_HOST = "localhost";
+    public static final String MSQL_PORT = "3306";
+    public static final String MSQL_USER = "root";
+    public static final String MSQL_PASSWORD = "12345";
+    public static final String MSQL_DATABASE = "pikachu-game";
+
+    // Config Server
+    public static final int SERVER_PORT = 8080;
+}
