bf50613ecfae148f37a71da900daff43541717d1
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-06-12 02:07:34 +0700

    update config

--- server/src/configs/Configs.java
+++ server/src/configs/Configs.java
@@ -12,7 +12,7 @@ public class Configs {
     public static final int SERVER_PORT = 8080;
 
     // Config Game
-    public static final int LEVEL = 10; // 1 -> 36
+    public static final int LEVEL = 13; // 1 -> 36
     public static final int NROOM = 1000;
     public static final int SCORE = 100;
     public static final int SCORE_BONUS = 72;
